731c15f75cc17429fe6087338938b684186b80d3
FotoAleph <fotoaleph@gmail.com>
2022-11-14 10:57:18 -0500

    Bootstrap install

--- MascotaFeliz/Taller/recursos/funciones.js
+++ MascotaFeliz/Taller/recursos/funciones.js
@@ -8,8 +8,9 @@
     // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
+        console.log(form.checkValidity());
         if (!form.checkValidity()) {
-          event.preventDefault()
+          event.preventDefault();
           event.stopPropagation()
         }else{
             RegistrarPersona();
@@ -20,31 +21,35 @@
         form.classList.add('was-validated')
       }, false)
     })
-  })()
+  })();
 
 function RegistrarPersona(){
-    let nombres = document.querySelector("#txtNombres").value;
-    let apellidos = document.querySelector("#txtApellidos").value;
+   
+    let cedula = document.querySelector("#txtDocumento").value;
+    let nombre = document.querySelector("#txtNombre").value;
+    let apellido = document.querySelector("#txtApellidos").value;
     let correo = document.querySelector("#txtCorreo").value;
-    let celular = document.querySelector("#txtCelular").value;
+    let telefono = document.querySelector("#txtCelular").value;
+     
 
-
-    let url = `http://localhost:3000/personas`;
+    let url = `http://localhost:3000/usuarios`;
     let datos = {
-        nombres: nombres,
-        apellidos: apellidos,
+        cedula:cedula,
+        nombre: nombre,
+        apellido: apellido,
         correo: correo,
-        celular: celular
+        telefono: telefono,
+        rol:"cliente"
+            };
 
-    };
-
+            console.log(JSON.stringify(datos));
     fetch(url,{
         method: 'POST',
         body: JSON.stringify(datos),
         headers:{
             'Content-Type':'application/json'
         }
-    }),then(res => res.json())
+    }).then(res => res.json())
     .then(mensaje =>{
         console.log(mensaje)
     })
